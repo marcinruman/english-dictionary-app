404cd3d31fd1424491208160ab965a14a52a81e8
Marcin Ruman <rumanmarcin@gmail.com>
2017-02-08 14:46:30 +0100

    Webpack Config Update + SASS files

--- src/js/Layout.js
+++ src/js/Layout.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+// Stylesheets
+require("../sass/main.scss");
+
 export default class Layout extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Hello World!</h1>
+            <div className="container container--body">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <h1 className="container__title">Hello World!</h1>
+                        <p className="container__desc">Welcome to Mini English Dictionary App</p>
+                    </div>
+                </div>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var path = require('path');
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require("path");
+
+const sassLoader = debug ?
+
+    ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
+
+    : 	ExtractTextPlugin.extract({
+            loader: ["css-loader?minimize", "postcss-loader", "sass-loader"]
+        });
 
 module.exports = {
     context: path.join(__dirname, "src"),
@@ -19,16 +27,30 @@ module.exports = {
                     presets: ['react', 'es2015', 'stage-0'],
                     plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties', 'transform-react-jsx-img-import']
                 }
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loaders: ['file-loader?limit=5000&name=images/[name]-[hash:6].[ext]'],
+                exclude: /(node_modules|bower_components)/
+            },
+            {
+                test: /\.(scss)$/,
+                loader: sassLoader
             }
         ]
     },
     output: {
         path: __dirname + "/dist",
         filename: "client.min.js",
-        publicPath: debug ? "/" : "/dist",
+        publicPath: "/",
     },
     plugins: debug ? [] : [
         new ExtractTextPlugin("style.min.css"),
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
         new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false})
     ]
 };
